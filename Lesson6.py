4478b15aec978bdbea2674249b4f526888293c2d
StevenWong <1014683756@qq.com>
2019-03-01 15:07:11 +0800

    Create Lesson6.py

---
+++ Lesson6.py
@@ -0,0 +1,46 @@
+#线性SVM和 Logistic 回归在 forge 数据集上的决策边界
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+import mglearn
+import matplotlib.pyplot as plt
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()],axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:,0],X[:,1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+axes[0].legend()
+
+mglearn.plots.plot_linear_svc_regularization()
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+cancer  = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+logreg = LogisticRegression().fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg.score(X_test, y_test)))
+
+logreg100 =  LogisticRegression(C=100).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg100.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+logreg001 = LogisticRegression(C=0.01).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+plt.plot(logreg.coef_.T, 'o', label="C=1")
+plt.plot(logreg100.coef_.T, '^', label="C=100")
+plt.plot(logreg001.coef_.T, 'v', label="C=0.001")
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+plt.hlines(0, 0, cancer.data.shape[1])
+plt.ylim(-5, 5)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.legend()
